6c6936d3c720e7676194f60a0d38211a91fcc965
Lily <liyo7397@colorado.edu>
2020-08-26 07:50:39 -0600

    Updates

---
+++ precache-manifest.f856e323276d3faa697c7250880a1674.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f6697d5db45ff7a8ef212728723654",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3f9a9338201a0eec4444",
+    "url": "/react-gh-pages/static/css/2.02d7b705.chunk.css"
+  },
+  {
+    "revision": "562f28213a189a2d37cc",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f9a9338201a0eec4444",
+    "url": "/react-gh-pages/static/js/2.c8665ec5.chunk.js"
+  },
+  {
+    "revision": "caa91be03acb57be55f3d42de5f8c39a",
+    "url": "/react-gh-pages/static/js/2.c8665ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562f28213a189a2d37cc",
+    "url": "/react-gh-pages/static/js/main.5d9d332e.chunk.js"
+  },
+  {
+    "revision": "1caab8dc5f48a18416c9",
+    "url": "/react-gh-pages/static/js/runtime-main.08eec64a.js"
+  },
+  {
+    "revision": "e0aa6af32e2da3aaa2c723aa1f35e511",
+    "url": "/react-gh-pages/static/media/CV.e0aa6af3.pdf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/react-gh-pages/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/react-gh-pages/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/react-gh-pages/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/react-gh-pages/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "cc55f80b8714bbf7b076a7c6a49dcd67",
+    "url": "/react-gh-pages/static/media/personal.cc55f80b.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1931b2bcaac2988ad989ac98ee2025e9.js"
+  "/react-gh-pages/precache-manifest.f856e323276d3faa697c7250880a1674.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
